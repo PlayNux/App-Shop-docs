f30e2d8e3c6e78c16616eb81995d7f6321732b82
Dumbo-programmer <rony.120285d2k@gmail.com>
2022-11-03 22:33:56 +0600

    test changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PlayNux'
+copyright = '2022, Tawhid Bin Omar'
+author = 'Tawhid Bin Omar'
 
 release = '0.1'
 version = '0.1.0'
